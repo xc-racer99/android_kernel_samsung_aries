57f9f466f37df795f368f168779f88d2fe01f0c9
stratosk <stratosk@albert.(none)>
2012-04-11 14:06:42 +0300

    Reenable touch wake when proximity off

--- drivers/input/misc/gp2a.c
+++ drivers/input/misc/gp2a.c
@@ -373,9 +373,12 @@ irqreturn_t gp2a_irq_handler(int irq, void *data)
 pr_info("proximity %u\n", proximity);
 
 #ifdef CONFIG_TOUCH_WAKE
-    if (!val) {
-        proximity_detected();
-    }
+	if (!val) {
+		proximity_detected();
+	}
+	else {
+		proximity_off();
+	}
 #endif
 
 	/* 0 is close, 1 is far */
