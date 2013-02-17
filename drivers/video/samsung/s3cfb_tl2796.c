3955b2640d1e3c5613ee745d24da0bb70610af88
DerTeufel <dominik-kassel@gmx.de>
2013-02-17 21:23:35 +0100

    fixing voodoo color?

--- drivers/video/samsung/s3cfb_tl2796.c
+++ drivers/video/samsung/s3cfb_tl2796.c
@@ -98,7 +98,6 @@ unsigned int panel_config_sequence = 0;
 
 int hacky_v1_offset[3] = {0, 0, 0};
 
-
 static const u16 s6e63m0_SEQ_ETC_SETTING_SAMSUNG[] = {
 	/* ETC Condition Set Command  */
 	0x0F6,
