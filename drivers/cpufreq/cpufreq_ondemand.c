d3c27fd8f77d50af48acab5d14a0882d0dd3bcae
stratosk <stratosk@semaphore.gr>
2013-02-21 22:47:12 +0200

    cpufreq: ondemand: Calculate gradient of CPU load to early increase frequency
    Instead of checking only the absolute value of CPU load_freq to increase
    frequency, we detect forthcoming CPU load rise and increase frequency
    earlier.
    
    Every sampling rate, we calculate the gradient of load_freq. If it is
    too steep we assume that the load most probably will go over
    up_threshold in next iteration(s) and we increase frequency immediately.
    
    New tuners are introduced:
    - early_demand: to enable this functionality (disabled by default).
    - grad_up_threshold: over this gradient of load we will increase
    frequency immediately.
    
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -33,11 +33,13 @@
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
+#define DEF_GRAD_UP_THRESHOLD			(50)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)
 #define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(10000)
 #define MIN_FREQUENCY_UP_THRESHOLD		(11)
 #define MAX_FREQUENCY_UP_THRESHOLD		(100)
+
 #define DEFAULT_FREQ_BOOST_TIME			(500000)
 #define MAX_FREQ_BOOST_TIME			(5000000)
 
@@ -66,6 +68,7 @@ static int status_old;
 #define MIN_SAMPLING_RATE_RATIO			(2)
 
 static unsigned int min_sampling_rate;
+static unsigned int orig_sampling_rate;
 
 #define LATENCY_MULTIPLIER			(1000)
 #define MIN_LATENCY_MULTIPLIER			(100)
@@ -100,6 +103,7 @@ struct cpu_dbs_info_s {
 	unsigned int freq_lo_jiffies;
 	unsigned int freq_hi_jiffies;
 	unsigned int rate_mult;
+	unsigned int prev_load_freq;
 	int cpu;
 	unsigned int sample_type:1;
 	/*
@@ -124,19 +128,25 @@ static struct dbs_tuners {
 	unsigned int down_differential;
 	unsigned int ignore_nice;
 	unsigned int sampling_down_factor;
+	unsigned int grad_up_threshold;
 	unsigned int powersave_bias;
 	unsigned int io_is_busy;
 	unsigned int boosted;
 	unsigned int freq_boost_time;
 	unsigned int boostfreq;
+	unsigned int early_demand;
 } dbs_tuners_ins = {
 	.up_threshold = DEF_FREQUENCY_UP_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
+
 	.down_differential = DEF_FREQUENCY_DOWN_DIFFERENTIAL,
+
 	.ignore_nice = 0,
 	.powersave_bias = 0,
 	.freq_boost_time = DEFAULT_FREQ_BOOST_TIME,
 	.boostfreq = 0,
+	.grad_up_threshold = DEF_GRAD_UP_THRESHOLD,
+	.early_demand = 0,
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
@@ -173,6 +183,7 @@ static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
 }
 
 static inline cputime64_t get_cpu_iowait_time(unsigned int cpu, cputime64_t *wall)
+
 {
 	u64 iowait_time = get_cpu_iowait_time_us(cpu, wall);
 
@@ -276,8 +287,9 @@ show_one(sampling_down_factor, sampling_down_factor);
 show_one(ignore_nice_load, ignore_nice);
 show_one(powersave_bias, powersave_bias);
 show_one(boostpulse, boosted);
-show_one(boosttime, freq_boost_time);
 show_one(boostfreq, boostfreq);
+show_one(grad_up_threshold, grad_up_threshold);
+show_one(early_demand, early_demand);
 
 static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
 				   const char *buf, size_t count)
@@ -288,6 +300,7 @@ static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
 	if (ret != 1)
 		return -EINVAL;
 	dbs_tuners_ins.sampling_rate = max(input, min_sampling_rate);
+	orig_sampling_rate = dbs_tuners_ins.sampling_rate;
 	return count;
 }
 
@@ -315,6 +328,10 @@ static ssize_t store_up_threshold(struct kobject *a, struct attribute *b,
 			input < MIN_FREQUENCY_UP_THRESHOLD) {
 		return -EINVAL;
 	}
+
+
+
+
 	dbs_tuners_ins.up_threshold = input;
 	return count;
 }
@@ -329,6 +346,7 @@ static ssize_t store_sampling_down_factor(struct kobject *a,
 	if (ret != 1 || input > MAX_SAMPLING_DOWN_FACTOR || input < 1)
 		return -EINVAL;
 	dbs_tuners_ins.sampling_down_factor = input;
+
 
 	/* Reset down sampling multiplier in case it was active */
 	for_each_online_cpu(j) {
@@ -390,7 +408,6 @@ static ssize_t store_powersave_bias(struct kobject *a, struct attribute *b,
 	return count;
 }
 
-
 static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
 				const char *buf, size_t count)
 {
@@ -423,6 +440,35 @@ static ssize_t store_boostfreq(struct kobject *a, struct attribute *b,
 	return count;
 }
 
+static ssize_t store_grad_up_threshold(struct kobject *a,
+			struct attribute *b, const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret != 1 || input > MAX_FREQUENCY_UP_THRESHOLD ||
+			input < MIN_FREQUENCY_UP_THRESHOLD) {
+		return -EINVAL;
+	}
+
+	dbs_tuners_ins.grad_up_threshold = input;
+	return count;
+}
+
+static ssize_t store_early_demand(struct kobject *a, struct attribute *b,
+				  const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+
+	ret = sscanf(buf, "%u", &input);
+	if (ret != 1)
+		return -EINVAL;
+	dbs_tuners_ins.early_demand = !!input;
+	return count;
+}
+
 define_one_global_rw(sampling_rate);
 define_one_global_rw(io_is_busy);
 define_one_global_rw(up_threshold);
@@ -431,6 +477,8 @@ define_one_global_rw(ignore_nice_load);
 define_one_global_rw(powersave_bias);
 define_one_global_rw(boostpulse);
 define_one_global_rw(boostfreq);
+define_one_global_rw(grad_up_threshold);
+define_one_global_rw(early_demand);
 
 static struct attribute *dbs_attributes[] = {
 	&sampling_rate_min.attr,
@@ -442,6 +490,8 @@ static struct attribute *dbs_attributes[] = {
 	&io_is_busy.attr,
 	&boostpulse.attr,
 	&boostfreq.attr,
+	&grad_up_threshold.attr,
+	&early_demand.attr,
 	NULL
 };
 
@@ -470,6 +520,8 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	struct cpufreq_policy *policy;
 	unsigned int j;
 	unsigned int boostfreq;
+
+
 
 	this_dbs_info->freq_lo = 0;
 	policy = this_dbs_info->cur_policy;
@@ -506,6 +558,7 @@ else{
 status_old = status;
 #endif
 
+	int boost_freq = 0;
 
 	/* Only core0 controls the boost */
 	if (dbs_tuners_ins.boosted && policy->cpu == 0) {
@@ -543,7 +596,10 @@ status_old = status;
 		j_dbs_info = &per_cpu(od_cpu_dbs_info, j);
 
 		cur_idle_time = get_cpu_idle_time(j, &cur_wall_time);
-		cur_iowait_time = get_cpu_iowait_time(j, &cur_wall_time);
+
+		if (dbs_tuners_ins.io_is_busy)
+			cur_iowait_time =
+				 get_cpu_iowait_time(j, &cur_wall_time);
 
 		wall_time = (unsigned int) cputime64_sub(cur_wall_time,
 				j_dbs_info->prev_cpu_wall);
@@ -553,9 +609,12 @@ status_old = status;
 				j_dbs_info->prev_cpu_idle);
 		j_dbs_info->prev_cpu_idle = cur_idle_time;
 
-		iowait_time = (unsigned int) cputime64_sub(cur_iowait_time,
+		if (dbs_tuners_ins.io_is_busy) {
+			iowait_time =
+				(unsigned int) cputime64_sub(cur_iowait_time,
 				j_dbs_info->prev_cpu_iowait);
-		j_dbs_info->prev_cpu_iowait = cur_iowait_time;
+			j_dbs_info->prev_cpu_iowait = cur_iowait_time;
+		}
 
 		if (dbs_tuners_ins.ignore_nice) {
 			cputime64_t cur_nice;
@@ -598,13 +657,27 @@ status_old = status;
 			max_load_freq = load_freq;
 	}
 
+	/*
+	 * Calculate the gradient of load_freq. If it is too steep we assume
+	 * that the load will go over up_threshold in next iteration(s) and
+	 * we increase the frequency immediately
+	 */
+	if (dbs_tuners_ins.early_demand) {
+		if (max_load_freq > this_dbs_info->prev_load_freq &&
+		   (max_load_freq - this_dbs_info->prev_load_freq >
+		    dbs_tuners_ins.grad_up_threshold * policy->cur))
+			boost_freq = 1;
+			this_dbs_info->prev_load_freq = max_load_freq;
+	}
+
 	/* Check for frequency increase */
 
 #ifdef CONFIG_DEVIL_TWEAKS
- if(smooth_ui() && touch_state_val){
+ if((smooth_ui() && touch_state_val) || boost_freq) {
     if(policy->cur < cpuL3freq() && cpuL3freq() <= policy->max)
     dbs_freq_increase(policy, cpuL3freq());
     else if(cpuL3freq() > policy->max){
+    /* If switching to max speed, apply sampling_down_factor */
     this_dbs_info->rate_mult = dbs_tuners_ins.sampling_down_factor;
     dbs_freq_increase(policy, policy->max);
     }
@@ -612,11 +685,9 @@ status_old = status;
     dbs_freq_increase(policy, cpuL3freq());
   return;
   }
-
   else
 #endif
-  if (max_load_freq > dbs_tuners_ins.up_threshold * policy->cur) {
-
+  if (max_load_freq > dbs_tuners_ins.up_threshold * policy->cur || boost_freq) {
 
 	/* If switching to max speed, apply sampling_down_factor */
 		if (policy->cur < policy->max)
@@ -631,6 +702,7 @@ status_old = status;
 		dbs_freq_increase(policy, boostfreq);
 		return;
 	}
+
 
 	/* Check for frequency decrease */
 	/* if we cannot reduce the frequency anymore, break out early */
@@ -664,6 +736,7 @@ status_old = status;
     else
   #endif
 	if (dbs_tuners_ins.boosted && freq_next < boostfreq) {
+
       	freq_next = boostfreq;
 		}
 		/* No longer fully busy, reset rate_mult */
@@ -764,6 +837,22 @@ static int should_io_be_busy(void)
 	return 0;
 }
 
+static void powersave_early_suspend(struct early_suspend *handler)
+{
+	dbs_tuners_ins.sampling_rate *= 4;
+}
+
+static void powersave_late_resume(struct early_suspend *handler)
+{
+	dbs_tuners_ins.sampling_rate = orig_sampling_rate;
+}
+
+static struct early_suspend _powersave_early_suspend = {
+	.suspend = powersave_early_suspend,
+	.resume = powersave_late_resume,
+	.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
+};
+
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				   unsigned int event)
 {
@@ -796,6 +885,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		}
 		this_dbs_info->cpu = cpu;
 		this_dbs_info->rate_mult = 1;
+		this_dbs_info->prev_load_freq = 0;
 		ondemand_powersave_bias_init_cpu(cpu);
 		/*
 		 * Start the timerschedule work, when this governor
@@ -821,12 +911,15 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			dbs_tuners_ins.sampling_rate =
 				max(min_sampling_rate,
 				    latency * LATENCY_MULTIPLIER);
+			orig_sampling_rate = dbs_tuners_ins.sampling_rate;
 			dbs_tuners_ins.io_is_busy = should_io_be_busy();
+
 		}
 		mutex_unlock(&dbs_mutex);
 
 		mutex_init(&this_dbs_info->timer_mutex);
 		dbs_timer_init(this_dbs_info);
+		register_early_suspend(&_powersave_early_suspend);
 		break;
 
 	case CPUFREQ_GOV_STOP:
@@ -840,6 +933,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			sysfs_remove_group(cpufreq_global_kobject,
 					   &dbs_attr_group);
 
+		unregister_early_suspend(&_powersave_early_suspend);
 		break;
 
 	case CPUFREQ_GOV_LIMITS:
