d9b2e109a402ce1415bd5e874f2833b1b3b7332e
Jonathan Bakker <xc-racer2@live.ca>
2018-09-12 10:48:52 -0700

    Remove most of the custom partition layout handling
    Leave partition layouts as fallback if no cmdline partitions

--- drivers/mtd/onenand/samsung.c
+++ drivers/mtd/onenand/samsung.c
@@ -30,9 +30,6 @@
 
 #include <linux/io.h>
 
-#include <asm/setup.h>
-#include <linux/string.h>
-
 enum soc_type {
 	TYPE_S3C6400,
 	TYPE_S3C6410,
@@ -217,56 +214,6 @@ static void s3c_dump_reg(void)
 	}
 }
 #endif
-
-struct slsi_ptbl_entry {
-	char name[16];
-	__u32 offset;
-	__u32 size;
-	__u32 flags;
-};
-
-struct mtd_partition *partitions;
-int num_partitions;
-
-#define MAX_PARTITIONS 12
-#define ATAG_SLSI_PARTITION 0x28247574
-struct mtd_partition slsi_nand_partitions[MAX_PARTITIONS];
-char slsi_nand_names[MAX_PARTITIONS * 16];
-
-static int __init parse_tag_partition(const struct tag *tag)
-{
-	struct mtd_partition *ptn = slsi_nand_partitions;
-	char *name = slsi_nand_names;
-	struct slsi_ptbl_entry *entry = (void *) &tag->u;
-	unsigned count, n;
-
-	count = (tag->hdr.size - 2) /
-		(sizeof(struct slsi_ptbl_entry) / sizeof(__u32));
-
-	if (count > MAX_PARTITIONS)
-		count = MAX_PARTITIONS;
-
-	for (n = 0; n < count; n++) {
-		memcpy(name, entry->name, 15);
-		name[15] = 0;
-		ptn->name = name;
-		ptn->offset = entry->offset;
-		ptn->size = entry->size;
-
-		printk(KERN_INFO "Partition (from atag) %15s -- Offset:0x%08x Size:0x%08x\n",
-				entry->name, entry->offset, entry->size);
-
-		name += 16;
-		entry++;
-		ptn++;
-	}
-
-	num_partitions = count;
-	partitions = slsi_nand_partitions;
-
-	return 0;
-}
-__tagtable(ATAG_SLSI_PARTITION, parse_tag_partition);
 
 static unsigned int s3c64xx_cmd_map(unsigned type, unsigned val)
 {
@@ -1103,35 +1050,13 @@ static int s3c_onenand_probe(struct platform_device *pdev)
 	if (s3c_read_reg(MEM_CFG_OFFSET) & ONENAND_SYS_CFG1_SYNC_READ)
 		dev_info(&onenand->pdev->dev, "OneNAND Sync. Burst Read enabled\n");
 
-#ifdef CONFIG_MTD_CMDLINE_PARTS
 	err = parse_mtd_partitions(mtd, part_probes, &onenand->parts, 0);
 	if (err > 0)
 		mtd_device_register(mtd, onenand->parts, err);
 	else if (err <= 0 && pdata && pdata->parts)
 		mtd_device_register(mtd, pdata->parts, pdata->nr_parts);
 	else
-#endif
-	if (num_partitions <= 0) {
-		/* default partition table */
-		num_partitions = ARRAY_SIZE(s3c_partition_info);	/* pdata->nr_parts */
-		partitions = s3c_partition_info;			/* pdata->parts */
-	}
-
-	if (partitions && num_partitions > 0)
-		err = mtd_device_register(mtd, partitions, num_partitions);
-	else
-		err = mtd_device_register(mtd, NULL, 0);
-
-
-/*
-	err = parse_mtd_partitions(mtd, part_probes, &onenand->parts, 0);
-	if (err > 0)
-		mtd_device_register(mtd, onenand->parts, err);
-	else if (err <= 0 && pdata && pdata->parts)
-		mtd_device_register(mtd, pdata->parts, pdata->nr_parts);
-	else
-		err = mtd_device_register(mtd, NULL, 0);
-*/
+		mtd_device_register(mtd, s3c_partition_info, ARRAY_SIZE(s3c_partition_info));
 
 	platform_set_drvdata(pdev, mtd);
 
